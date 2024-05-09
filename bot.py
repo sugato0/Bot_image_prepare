42ea03ddea187902fd3c37ef7a6a85fd27a1d258
Sugato0 <sugatokavary@gmail.com>
2024-05-09 22:21:26 +0200

    fix = сделано ответная отправка обработанного изображения попытка сделать mediagroup безумпешна

--- bot.py
+++ bot.py
@@ -1,13 +1,17 @@
+from typing import Union
+
 from aiogram import Bot,Dispatcher,types
 from handlers.image_prepare import router as img_prepare_router
 import asyncio
 from config import config_sets
 import logging
+from aiogram.handlers import ErrorHandler
+from aiogram import BaseMiddleware
+
 bot = Bot(token=config_sets.token.get_secret_value())
 dp = Dispatcher()
 
 logging.basicConfig(level = logging.INFO)
-
 
 async def main():
     dp.include_routers(img_prepare_router)
--- handlers/image_prepare.py
+++ handlers/image_prepare.py
@@ -11,14 +11,9 @@ from glob import glob
 from aiogram.utils.media_group import MediaGroupBuilder
 import cv2
 from PIL import Image
-import os
-import shutil
-
 from keyboards import get_next_btn
-class Get_images(StatesGroup):
-    waiting_images = State()
 router = Router()
-
+media_groups = []
 @router.message(Command("start"))
 async def cmd_start(message:types.Message,state: FSMContext):
     # await state.set_state(User.waiting_name.state)
@@ -40,35 +35,29 @@ async def cmd_start(message:types.Message,state: FSMContext):
 async def send_photo(call: types.CallbackQuery,state: FSMContext):
     image_callback = call.data.split("_")[-1]
     data = await state.get_data()
-    await state.set_state(Get_images.waiting_images.state)
     if "event" in data:
         await state.update_data({"event": data["event"]+image_callback})
     else:
         await state.update_data({"event": image_callback})
     await call.answer(text=f"Загрузите фотографии (группой или одно) ")
 
-@router.message(Get_images.waiting_images)
+
+@router.message(F.photo)
 async def photo_prepare(message: types.Message,state: FSMContext,bot:Bot):
     try:
-        file = message.photo
-        file_path = await bot.get_file(file[-1].file_id)
+        if message.media_group_id in media_groups:
+            return
+        media_groups.append(message.media_group_id)
 
+        files = message.photo
+        file_path = await bot.get_file(files[-1].file_id)
         bin_img = io.BytesIO()
         data = await state.get_data()
-        print(file_path)
-        # path = r'docs/' + file_path.file_path+ '.jpg'
-        buf_input = types.FSInputFile(
-            file_path.file_unique_id,
-            filename=file_path.file_path
-        )
-        print(buf_input.filename)
-        filename = file_path.file_path.split("/")[-1]
+
         await bot.download_file(
             file_path.file_path,
             bin_img
         )
-        file = None
-
         buf_file = np.frombuffer(
             bin_img.getvalue(),
             np.uint8
@@ -77,121 +66,28 @@ async def photo_prepare(message: types.Message,state: FSMContext,bot:Bot):
         if data["event"] == "grayscale":
             img = cv2.imdecode(buf_file, cv2.IMREAD_GRAYSCALE)
         img = Image.fromarray(img)
-        print(img)
-        # img.save(img, format=filename.split(".")[-1])
-        # img.seek(0)
-        # return buf
-        #с этого моента нужно разобраться как отправить массив
 
-        input_img = types.InputFile(
-            img,
-            filename=filename
-
+        with io.BytesIO() as output:
+            img.save(output, format='JPEG')  # You can change the format if needed
+            img_bytes = output.getvalue()
+        buf_photo = types.BufferedInputFile(
+            file=img_bytes,
+            filename="new file.jpg"
         )
+
+
         await bot.send_photo(
             message.chat.id,
-            input_img
+            media=buf_photo
         )
-
-
-
-        # photos = glob(f"./before_prepare_images/{message.from_user.id}/*.jpg")
+        # await bot.send_photo(
+        #     message.chat.id,
         #
-        # album_builder = MediaGroupBuilder(
-        #     caption="Преобразованные изображения"
         # )
-        # data = await state.get_data()
-        #
-        # if "event" not in data:
-        #     await message.answer(text=f"Повторите вход через /start")
-        #     shutil.rmtree(f"before_prepare_images/{message.from_user.id}")
-        #     return
-        # for photo in photos:
-        #     #главная обработка как тебе нужно
-        #     file = None
-        #     if data["event"] == "grayscale":
-        #         file = cv2.imread(photo[2:], cv2.IMREAD_GRAYSCALE)
-        #         if not all(file):
-        #             await message.answer(text=f"внутренний сбой, повторите вход через /start")
-        #             return
-        #     try:
-        #         album_builder.add(
-        #             type="photo",
-        #             media=types.BufferedInputFile(bytearray(file),filename=f"{random.randint(1000,10000000)}.jpg")
-        #         )
-        #     except PermissionError:
-        #         print("PermissionError: The file is being used by another process. Waiting for 5 seconds...")
-        #         time.sleep(5)
-        # await message.answer_media_group(
-        #     # Не забудьте вызвать build()
-        #     media=album_builder.build()
-        # )
-        # shutil.rmtree(f"before_prepare_images/{message.from_user.id}")
+
+
     except Exception as e:
         print(e)
 
-
-
 #
 #
-# @router.message(User.waiting_name,F.text)
-# async def waiting_name(message: types.Message, state: FSMContext):
-#     if "1" in message.text:
-#         await message.answer("Ошибка ввода 1 не должно быть")
-#         return
-#
-#     await state.update_data(name=message.text) # a['name'] = message.text
-#     await state.set_state(User.waiting_surname.state)
-#
-#     await message.answer(f"Ура ваше имя <b>{message.text}</b> введите фамилию",
-#                          parse_mode="HTML")
-#
-# @router.message(User.waiting_surname,F.text)
-# async def waiting_surname(message: types.Message, state: FSMContext):
-#     if "1" in message.text:
-#         await message.answer("Ошибка ввода 1 не должно быть")
-#         return
-#
-#     await state.update_data(surname=message.text)
-#     await state.set_state(User.waiting_photo.state)
-#     data = await state.get_data()
-#
-#     await message.answer(f"Ура ваше имя <b>{data['name']}</b> \n Ура ваша фамилия {data['surname']}",
-#                          parse_mode="HTML")
-#
-# @router.callback_query(F.data == "Груша")
-# async def names_prepare(call: types.CallbackQuery):
-#     await call.answer(text=f"Вы нажали на {call.data}",show_allert = True)
-# @router.message(User.waiting_photo,F.photo)
-# async def waiting_name(message: types.Message, state: FSMContext):
-#
-#
-#     await state.set_state(None)
-#     await state.clear()
-#     await message.answer(f"Ура фото тут",
-#                          parse_mode="HTML")
-#
-#
-#
-#
-
-
-
-# @router.message(F.photo)
-# async def download_photo(message: types.Message,bot: Bot):
-#
-#
-#     await bot.download(
-#         message.photo[-1],
-#         destination=f'saved_images/{message.photo[-1].file_id}.jpg'
-#     )
-#     path = f"saved_images/{message.photo[-1].file_id}.jpg"
-#     file = cv2.imread(path,cv2.IMREAD_GRAYSCALE)
-#     cv2.imwrite(f"new_{message.photo[-1].file_id}.jpg",file)
-#     path1 = f"new_{message.photo[-1].file_id}.jpg"
-#     image = types.FSInputFile(path1)
-#
-#     await message.answer_photo(photo=image,
-#                                caption="Вот изображение забирай обратно")
-
-
